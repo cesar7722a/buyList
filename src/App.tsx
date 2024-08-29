60222c676fe0ccc6566d92f98e8ce912790297b1
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-29 20:05:25 +0100

    chose:remove div q estavam a mais

--- src/App.tsx
+++ src/App.tsx
@@ -11,55 +11,51 @@ export function App() {
       <div className="space-y-8">
         <CardText />
         <FormAdd />
-        <div>
-          <ul className="space-y-3">
-            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-              <input
-                type="checkbox"
-                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-              />
-              <p className="flex-1 text-[#374151] font-medium">Pão de forma</p>
-              <span className="p-2">
-                <Trash2 className="size-5 text-[#9CA3AF]" />
-              </span>
-            </li>
+        <ul className="space-y-3">
+          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+            <input
+              type="checkbox"
+              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+            />
+            <p className="flex-1 text-[#374151] font-medium">Pão de forma</p>
+            <span className="p-2">
+              <Trash2 className="size-5 text-[#9CA3AF]" />
+            </span>
+          </li>
 
-            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-              <input
-                type="checkbox"
-                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-              />
-              <p className="flex-1 text-[#374151] font-medium">Café Preto</p>
-              <span className="p-2">
-                <Trash2 className="size-5 text-[#9CA3AF]" />
-              </span>
-            </li>
+          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+            <input
+              type="checkbox"
+              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+            />
+            <p className="flex-1 text-[#374151] font-medium">Café Preto</p>
+            <span className="p-2">
+              <Trash2 className="size-5 text-[#9CA3AF]" />
+            </span>
+          </li>
 
-            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-              <input
-                type="checkbox"
-                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-              />
-              <p className="flex-1 text-[#374151] font-medium">
-                Suco de laranja
-              </p>
-              <span className="p-2">
-                <Trash2 className="size-5 text-[#9CA3AF]" />
-              </span>
-            </li>
+          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+            <input
+              type="checkbox"
+              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+            />
+            <p className="flex-1 text-[#374151] font-medium">Suco de laranja</p>
+            <span className="p-2">
+              <Trash2 className="size-5 text-[#9CA3AF]" />
+            </span>
+          </li>
 
-            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-              <input
-                type="checkbox"
-                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-              />
-              <p className="flex-1 text-[#374151] font-medium">Bolacha</p>
-              <span className="p-2">
-                <Trash2 className="size-5 text-[#9CA3AF]" />
-              </span>
-            </li>
-          </ul>
-        </div>
+          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+            <input
+              type="checkbox"
+              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+            />
+            <p className="flex-1 text-[#374151] font-medium">Bolacha</p>
+            <span className="p-2">
+              <Trash2 className="size-5 text-[#9CA3AF]" />
+            </span>
+          </li>
+        </ul>
         <CardAlert />
       </div>
     </div>
