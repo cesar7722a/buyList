f51e9a78a2d1e368f0415b408c75b1aaa34ecc8c
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-29 18:51:32 +0100

    fea:criando a lista de compras

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Trash2 } from "lucide-react";
 import Logo from "../public/logo.png";
 
 export function App() {
@@ -23,10 +23,59 @@ export function App() {
             placeholder="Adicione um novo item"
             className="w-[451px] rounded-xl px-2 flex-1 py-2 bg-white border border-[#D1D5DB] hover:border-[#B9C2D0] focus:border-[#CA3884] outline-none"
           />
-          <button className="bg-[#CA3884] w-40 rounded-xl text-white hover:bg-[#A52C6B]">
+          <button className="bg-[#CA3884] font-semibold w-40 rounded-xl text-white hover:bg-[#A52C6B]">
             Adicionar item
           </button>
         </form>
+        <div>
+          <ul className="space-y-3">
+            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+              <input
+                type="checkbox"
+                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+              />
+              <p className="flex-1 text-[#374151] font-medium">Pão de forma</p>
+              <span className="p-2">
+                <Trash2 className="size-5 text-[#9CA3AF]" />
+              </span>
+            </li>
+
+            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+              <input
+                type="checkbox"
+                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+              />
+              <p className="flex-1 text-[#374151] font-medium">Café Preto</p>
+              <span className="p-2">
+                <Trash2 className="size-5 text-[#9CA3AF]" />
+              </span>
+            </li>
+
+            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+              <input
+                type="checkbox"
+                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+              />
+              <p className="flex-1 text-[#374151] font-medium">
+                Suco de laranja
+              </p>
+              <span className="p-2">
+                <Trash2 className="size-5 text-[#9CA3AF]" />
+              </span>
+            </li>
+
+            <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
+              <input
+                type="checkbox"
+                className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
+              />
+              <p className="flex-1 text-[#374151] font-medium">Bolacha</p>
+              <span className="p-2">
+                <Trash2 className="size-5 text-[#9CA3AF]" />
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
