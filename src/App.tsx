51038b2e832b2b5fe129144f658ce141b172e7fd
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-30 16:35:15 +0100

    feat:criei um array para a lista de compras

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import { CardAlert } from "./components/CardAlert";
 import { FormAdd } from "./components/FormAdd";
 import { CardText } from "./components/CardText";
 import { HeaderLogo } from "./components/HeaderLLogo";
-
+import { useImmer } from "use-immer";
+interface ListProps {
+  name: string;
+  id: number;
+}
 export function App() {
+  const [lists, updateLists] = useImmer<ListProps[]>([
+    { name: "Pão de forma", id: 1 },
+    { name: "Café preto", id: 2 },
+    { name: "Suco de laranja", id: 3 },
+    { name: "Bolacha", id: 4 },
+    { name: "Farinha de trigo", id: 5 },
+  ]);
   return (
     <div className="flex justify-center items-center flex-col gap-8 pb-4">
       <HeaderLogo />
@@ -12,46 +23,18 @@ export function App() {
         <CardText />
         <FormAdd />
         <ul className="space-y-3">
-          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-            <input type="checkbox" className="accent-[#CA3884]" />
-            <p className="flex-1 text-[#374151] font-medium">Pão de forma</p>
-            <span className="p-2">
-              <Trash2 className="size-5 text-[#9CA3AF]" />
-            </span>
-          </li>
-
-          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-            <input
-              type="checkbox"
-              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-            />
-            <p className="flex-1 text-[#374151] font-medium">Café Preto</p>
-            <span className="p-2">
-              <Trash2 className="size-5 text-[#9CA3AF]" />
-            </span>
-          </li>
-
-          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-            <input
-              type="checkbox"
-              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-            />
-            <p className="flex-1 text-[#374151] font-medium">Suco de laranja</p>
-            <span className="p-2">
-              <Trash2 className="size-5 text-[#9CA3AF]" />
-            </span>
-          </li>
-
-          <li className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white ">
-            <input
-              type="checkbox"
-              className="rounded-md border border-[#F4F5FB] text-white hover:border-[#B9C2D0] selection:text-[#C3884]"
-            />
-            <p className="flex-1 text-[#374151] font-medium">Bolacha</p>
-            <span className="p-2">
-              <Trash2 className="size-5 text-[#9CA3AF]" />
-            </span>
-          </li>
+          {lists.map((list) => (
+            <li
+              key={list.id}
+              className="flex items-center gap-3 px-3 py-2 rounded-xl bg-white "
+            >
+              <input type="checkbox" className="accent-[#CA3884]" />
+              <p className="flex-1 text-[#374151] font-medium">{list.name}</p>
+              <span className="p-2">
+                <Trash2 className="size-5 text-[#9CA3AF]" />
+              </span>
+            </li>
+          ))}
         </ul>
         <CardAlert />
       </div>
